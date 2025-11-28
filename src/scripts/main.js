acdf44ab1e794388eb118a2e83acc0538823d23b
Valentyna Chernova <valentyna19960410@gmail.com>
2025-11-28 23:06:05 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+const mainImg = document.querySelector('#largeImg');
+const anchor = document.querySelectorAll('a');
+anchor.forEach(a => {
+  a.addEventListener('click', function(e){
+    e.preventDefault();
+    const targetImg = e.target.closest('a');
+    if(targetImg) {
+      const source = targetImg.getAttribute('href');
+      mainImg.setAttribute('src', source);
+    }
+  })
+})
+
