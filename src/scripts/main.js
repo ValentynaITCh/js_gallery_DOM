be3f2e2e6e5b4e1986e3acb29f88c277ce7551c5
Valentyna Chernova <valentyna19960410@gmail.com>
2025-11-28 23:16:07 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,14 @@
 'use strict';
 
 const mainImg = document.querySelector('#largeImg');
-const anchor = document.querySelectorAll('a');
+const ulist= document.querySelector('ul');
 
-anchor.forEach((a) => {
-  a.addEventListener('click', function (e) {
+  ulist.addEventListener('click', function (e) {
     e.preventDefault();
 
     const targetImg = e.target.closest('a');
 
     if (targetImg) {
-      const source = targetImg.getAttribute('href');
-
-      mainImg.setAttribute('src', source);
+      mainImg.src = targetImg.href;
     }
   });
-});
