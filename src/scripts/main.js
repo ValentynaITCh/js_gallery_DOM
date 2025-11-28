56b6903b8d2a8ddecaa4e9114a6953f3bf8ffb19
Valentyna Chernova <valentyna19960410@gmail.com>
2025-11-28 23:08:29 +0200

    add demo link

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,18 @@
 'use strict';
+
 const mainImg = document.querySelector('#largeImg');
 const anchor = document.querySelectorAll('a');
-anchor.forEach(a => {
-  a.addEventListener('click', function(e){
+
+anchor.forEach((a) => {
+  a.addEventListener('click', function (e) {
     e.preventDefault();
+
     const targetImg = e.target.closest('a');
-    if(targetImg) {
+
+    if (targetImg) {
       const source = targetImg.getAttribute('href');
+
       mainImg.setAttribute('src', source);
     }
-  })
-})
-
+  });
+});
