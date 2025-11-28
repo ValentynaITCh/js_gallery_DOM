cb34199ad44cabdd7cc6e89de01e6fe278295e50
Valentyna Chernova <valentyna19960410@gmail.com>
2025-11-28 23:09:04 +0200

    make build

---
+++ index.6c0b11de.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelectorAll("a").forEach(function(t){t.addEventListener("click",function(t){t.preventDefault();var r=t.target.closest("a");if(r){var c=r.getAttribute("href");e.setAttribute("src",c)}})});
+//# sourceMappingURL=index.6c0b11de.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+"use strict";
+const mainImg = document.querySelector("#largeImg");
+const anchor = document.querySelectorAll("a");
+anchor.forEach((a)=>{
+    a.addEventListener("click", function(e) {
+        e.preventDefault();
+        const targetImg = e.target.closest("a");
+        if (targetImg) {
+            const source = targetImg.getAttribute("href");
+            mainImg.setAttribute("src", source);
+        }
+    });
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
